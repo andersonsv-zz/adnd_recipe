bc39adfcab179a18903b6e8e1aa5db58c16e4051
Your Name <anderson.si.vieira@hotmail.com>
2018-10-15 22:31:21 -0300

    Inclusão de libs e retrofit implementação

---
+++ app/src/main/java/br/com/andersonsv/recipe/network/RetrofitClientInstance.java
@@ -0,0 +1,19 @@
+package br.com.andersonsv.recipe.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+    private static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
