63e8d92bcc1f595b806495e1d8160afffeb8de9e
Your Name <anderson.si.vieira@hotmail.com>
2018-10-25 22:37:17 -0300

    Adicionado parcelable

---
+++ app/src/main/java/br/com/andersonsv/recipe/data/Ingredient.java
@@ -0,0 +1,44 @@
+package br.com.andersonsv.recipe.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Ingredient implements Parcelable {
+
+    private Double quantity;
+    private String measure;
+    private String ingredient;
+
+    public Ingredient() {
+    }
+
+    protected Ingredient(Parcel in) {
+        this.quantity = (Double) in.readValue(Double.class.getClassLoader());
+        this.measure = in.readString();
+        this.ingredient = in.readString();
+    }
+
+    public static final Creator<Ingredient> CREATOR = new Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel source) {
+            return new Ingredient(source);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.quantity);
+        dest.writeString(this.measure);
+        dest.writeString(this.ingredient);
+    }
+}
--- app/src/main/java/br/com/andersonsv/recipe/data/Recipe.java
+++ app/src/main/java/br/com/andersonsv/recipe/data/Recipe.java
@@ -1,4 +1,57 @@
 package br.com.andersonsv.recipe.data;
 
-public class Recipe {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
+
+public class Recipe implements Parcelable {
+
+    private Integer id;
+    private String name;
+    private List<Ingredient> ingredients = null;
+    private List<Step> steps = null;
+    private Integer servings;
+    private String image;
+
+    public Recipe() {
+    }
+
+    protected Recipe(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.name = in.readString();
+        this.ingredients = in.createTypedArrayList(Ingredient.CREATOR);
+        this.steps = in.createTypedArrayList(Step.CREATOR);
+        this.servings = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.image = in.readString();
+    }
+
+    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel source) {
+            return new Recipe(source);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.name);
+        dest.writeTypedList(this.ingredients);
+        dest.writeTypedList(this.steps);
+        dest.writeValue(this.servings);
+        dest.writeString(this.image);
+    }
+
 }
