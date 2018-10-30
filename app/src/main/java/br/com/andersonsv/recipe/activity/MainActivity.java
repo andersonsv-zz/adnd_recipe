8ee23382ae970fcfae3f23e273a86e0f122cffed
Your Name <anderson.si.vieira@hotmail.com>
2018-10-29 22:40:30 -0300

    Checagem de internet

--- app/src/main/java/br/com/andersonsv/recipe/activity/MainActivity.java
+++ app/src/main/java/br/com/andersonsv/recipe/activity/MainActivity.java
@@ -4,18 +4,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import br.com.andersonsv.recipe.R;
 import br.com.andersonsv.recipe.adapter.RecipeRecyclerViewAdapter;
 import br.com.andersonsv.recipe.data.Recipe;
 import br.com.andersonsv.recipe.network.RecipeService;
 import br.com.andersonsv.recipe.network.RetrofitClientInstance;
+import br.com.andersonsv.recipe.util.NetworkUtils;
 import br.com.andersonsv.recipe.util.UiUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,11 +29,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 public class MainActivity extends AppCompatActivity implements RecipeRecyclerViewAdapter.RecipeRecyclerOnClickHandler{
 
     private static final String TAG = MainActivity.class.getName();
 
     @BindView(R.id.rvRecipe) RecyclerView mRvRecipe;
+    @BindView(R.id.pgLoading) ProgressBar mLoading;
+    @BindView(R.id.mainContext) ConstraintLayout mMainContext;
 
     private RecipeRecyclerViewAdapter mRecipeAdapter;
 
@@ -41,34 +51,12 @@ public class MainActivity extends AppCompatActivity implements RecipeRecyclerVie
 
         context = this;
 
-        /*if (!NetworkUtils.isNetworkConnected(this)) {
-            mLlInternetAccessError.setVisibility(View.VISIBLE);
-            mErrorMessageDisplay.setVisibility(View.INVISIBLE);
-
-            Snackbar snackbar = Snackbar.make(movieActivity, R.string.offline_no_internet, Snackbar.LENGTH_LONG)
-                    .setAction(R.string.offline_no_internet_retry, new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            loadMovieData(movieSearch);
-                        }
-                    });
-            snackbar.show();
-
+        if (!NetworkUtils.isNetworkConnected(this)) {
+           // retry connect
+            createSnackbarToReload(R.string.offline_no_internet);
         } else {
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-
-            Bundle bundleForLoader = new Bundle();
-
-            if(movieSearch != null){
-                bundleForLoader.putInt(Intent.EXTRA_KEY_EVENT, movieSearch.ordinal());
-            }
-
-            LoaderManager.LoaderCallbacks<List<Movie>> callback = MovieActivity.this;
-
-            getSupportLoaderManager().restartLoader(MOVIE_LOADER_ID, bundleForLoader, callback);
-        }*/
-
-        loadData();
+            loadData();
+        }
     }
 
     private void loadData(){
@@ -76,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements RecipeRecyclerVie
 
         Call<ArrayList<Recipe>> call = service.getRecipes();
 
+        mLoading.setVisibility(View.VISIBLE);
 
         call.enqueue(new Callback<ArrayList<Recipe>>() {
             @Override
@@ -93,13 +82,28 @@ public class MainActivity extends AppCompatActivity implements RecipeRecyclerVie
                 mRvRecipe.setHasFixedSize(true);
 
                 mRvRecipe.setAdapter(mRecipeAdapter);
+
+                mLoading.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFailure(Call<ArrayList<Recipe>> call, Throwable t) {
+                mLoading.setVisibility(View.INVISIBLE);
+                createSnackbarToReload(R.string.error_download_data);
                 Log.e(TAG, "ERROR " + t.getLocalizedMessage());
             }
         });
+    }
+
+    private void createSnackbarToReload(int textResource){
+        Snackbar snackbar = Snackbar.make(mMainContext, textResource, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.retry_download_data, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        loadData();
+                    }
+                });
+        snackbar.show();
     }
 
     @Override
