a808fb825e722c2cee302bd1303a5410962f266d
Your Name <anderson.si.vieira@hotmail.com>
2018-11-08 22:42:19 -0200

    Alteracao nos testes e no widget

---
+++ app/src/androidTest/java/br/com/andersonsv/recipe/activity/MainActivityTest.java
@@ -0,0 +1,48 @@
+package br.com.andersonsv.recipe.activity;
+
+
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.com.andersonsv.recipe.R;
+
+import static org.hamcrest.Matchers.anything;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource() {
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        IdlingRegistry.getInstance().register(mIdlingResource);
+    }
+
+    @Test
+    public void idlingResourceTest() {
+        onData(anything()).inAdapterView(withId(R.id.rvRecipe)).atPosition(0).perform(click());
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
+    }
+}
